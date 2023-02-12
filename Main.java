d2fd63322695721ff195f82675ea75e5c4e3fab4
ApollonUmets <123029502+ApollonUmets@users.noreply.github.com>
2023-02-12 13:27:06 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+        static final double caloriesInSquareSmOfPizza = 10;
+
+        static double calculateArea(double diameter) {
+            double radius = diameter / 2;
+            return  radius * radius * Math.PI; }
+        static double calculatePizzaCalories(double diameter) {
+            double res = calculateArea(diameter) * caloriesInSquareSmOfPizza;
+            return res;
+        }
+        static void printResult(String calories, double amountCalories) {
+            System.out.println(calories + amountCalories);
+        }
+        public static void main(String[] args) {
+            double firstPizza = calculatePizzaCalories(20.0);
+            double secondPizza = calculatePizzaCalories(25.0);
+            printResult("First Pizza : ", firstPizza);
+            printResult("Second Pizza : ", secondPizza);
+            printResult("Pizza calories difference : ", secondPizza - firstPizza);
+        }
+    }
